48c13bf2b73827194eb44d5bf5a8137990a5fabb
Michael Kerber <mkerber@stanford.edu>
2013-07-07 19:02:56 +0000

    Bug

--- web.js
+++ web.js
@@ -5,9 +5,10 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var bf = fs.readFileSync('index.html');
   response.send(bf.toString());
-};
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
